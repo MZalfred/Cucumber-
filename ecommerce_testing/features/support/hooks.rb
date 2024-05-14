aaa4fbf7874a6d29405493e4b52b246a2df8b6b9
MZalfred <cutesy-todo0x@icloud.com>
2024-05-14 09:16:18 +0100

    Update hooks.rb

---
+++ ecommerce_testing/features/support/hooks.rb
@@ -0,0 +1,9 @@
+Before do
+  # Code to run before each scenario. For example, you could reset test data here.
+  puts "Starting a new scenario..."
+end
+
+After do
+  # Code to run after each scenario. For example, take a screenshot if a scenario fails.
+  puts "Finished scenario."
+end
