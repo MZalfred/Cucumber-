1397769789b2f6e15a057c27db5fe8f10cd8bbf9
MZalfred <cutesy-todo0x@icloud.com>
2024-05-16 09:27:44 +0100

    main

--- ecommerce_testing/features/step_definitions/browes_steps.rb
+++ ecommerce_testing/features/step_definitions/browes_steps.rb
@@ -1,10 +1,20 @@
 Given('I am on the homepage') do
-  @driver.navigate.to "http://example.com"
+  @driver.navigate.to "http://localhost:8000/product_page.html"
+  puts "Current URL: #{@driver.current_url}"
+  puts "Page Title: #{@driver.title}"
 end
 
+
 When('I click on a product') do
-  @driver.find_element(:css, "a.product_link").click
+  wait = Selenium::WebDriver::Wait.new(timeout: 10) # waits up to 10 seconds
+  product_link = wait.until {
+    element = @driver.find_element(:css, "a.product_link")
+    element if element.displayed?
+  }
+  puts "Element found, proceeding to click."
+  product_link.click
 end
+
 
 Then('I should see the product details') do
   details = @driver.find_element(:css, "div.product_details")
--- ecommerce_testing/features/support/env.rb
+++ ecommerce_testing/features/support/env.rb
@@ -1,15 +1,13 @@
-
-
 require 'selenium-webdriver'
 require 'cucumber'
 require 'rspec'
 
 Before do
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless') # Uncomment if you don't want the browser to open up
+  options.add_argument('--headless')  # Optional: Run Chrome in headless mode
   @driver = Selenium::WebDriver.for :chrome, options: options
 end
 
 After do
-  @driver.quit
+  @driver.quit if @driver  # Safely close the browser after each scenario
 end
